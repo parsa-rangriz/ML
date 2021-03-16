39d59edc84cd12ca0922fc38e40103ba18159aeb
Yasamin Panahi <79021790+YasaminPanahi@users.noreply.github.com>
2021-03-16 18:33:03 +0330

    Update Transversefield_Isingmodel_1D.m

--- Transversefield_Isingmodel_1D.m
+++ Transversefield_Isingmodel_1D.m
@@ -5,7 +5,7 @@ tic
 %           Transverse-field Ising model              
 %=============================================================
 %Parameters:
-N = 14; %number of sites
+N = 12; %number of sites
 J = 1;
 bnd = 1; %periodic bc = 1 & %open bc = 0
 
@@ -13,11 +13,10 @@ bnd = 1; %periodic bc = 1 & %open bc = 0
 sigma_x = [0 1;1 0];sigma_y = [0 -1i;1i 0];sigma_z = [1 0;0 -1];I = eye(2);
 X = sparse(sigma_x);Y = sparse(sigma_y);Z = sparse(sigma_z);I = sparse(I);
 
-first_h = 0; last_h = 5; steps = 0.1;
+first_h = 0; last_h = 7; steps = 0.1;
 Ns = ((last_h - first_h)/steps)+1; %Number of samples
 Nz = (N*(N-1))/2; %Number of correlation functions
-Nf = Nz + N +1;   %Number of features
-
+Nf = Nz + 2*N + 2^N +1;   %Number of features
 y = linspace(0,last_h,Ns); %'h' which is y in our data
 y = reshape(y,Ns,1);
 
@@ -40,7 +39,7 @@ for i=1:N
     end
 end
 
-%% Construction of mio Matrices
+%% Construction of mu Matrices
 mio = cell(1,N);
 X_cell = cell(1,N); X_cell{1} = X; I_cell=cell(1,N);
 for i=2:N
@@ -57,17 +56,22 @@ for i=1:N
         mio{i} = X_cell{i};
     end
 end
-
 %% One dimentional Quantum Ising model & Data generation:
 
 %Desired Quantities:
 G_Energy = zeros(1,Ns);    %Ground state energy 
-G_State = zeros(2^N,Ns);   %Ground state wave function 
+Energy_gap = zeros(Ns,1);  %Energy gap
+G_State = zeros(2^N,Ns);   %Ground state wave function
+G_Data = zeros(Ns,2^N);    %Ground state wave function 
 exp_Sx = zeros(Ns,1);      %Expectation values for Sx
 corr_SzSz = zeros(Ns,Nz);  %Correlation functions for Sz
 exp_mio = zeros(Ns,N);     %Expectation values for mio
+RDM = cell(1,N-1);         %Reduced Density matrix for each subsystem 
+Entanglemennt_Entropy = zeros(Ns,N-1); %Entanglemennt_Entropy
 Datas = zeros(Ns+1,Nf);    %Datas in form of (X,Y)
-k=0;
+k=0; M =0;
+
+%Main Loop:
 for h=first_h:steps:last_h
     k=k+1;
     
@@ -83,11 +87,28 @@ for h=first_h:steps:last_h
     %======================
     %   Diagonalization           
     %======================
-    [U,D] = eigs(H,5,'sa'); 
-    energy = diag(D); %Energy spectrum
+    [States,Energys] = eigs(H,3,'sa'); 
+    energy = diag(Energys); %Energy spectrum
     G_Energy(:,k) = energy(1); %Ground state energy
-    G_State(:,k) = U(:,1); %Ground state wavefunction
+    Energy_gap(k,1) = abs(energy(3) - G_Energy(:,k)); %Energy gap
+    G_State(:,k) = States(:,1);  %Ground state wavefunction
+    norm = abs(G_State(:,k)'*G_State(:,k));
+    G_State(:,k)= G_State(:,k)/norm; %normalization
     
+    %===========================
+    %   Entanglemennt Entropy         
+    %===========================
+    for i=1:N-1
+        N_left = i;
+        N_right = N - i;
+        psi = reshape(G_State(:,k),2^(N_right),2^(N_left));
+        RDM{i} = psi'*psi ;
+        [U,D]=eig(RDM{i});
+        p = diag(D);
+        p (p< 10^-15) = 10 ^ -15;
+        Entanglemennt_Entropy(k,i) = -dot(p,log(p));
+    end
+        
     %==================================================
     %   Expectation values & Correlation functions         
     %==================================================
@@ -99,77 +120,72 @@ for h=first_h:steps:last_h
             c = c + 1;
         end
         exp_mio(k,i) = (G_State(:,k)')*mio{i}*(G_State(:,k));
-    end  
+    end 
+    G_Data(k,:) = reshape(G_State(:,k),1,2^N);
     Datas(k+1,1:Nz) = corr_SzSz(k,:);
-    Datas(k+1,Nz+1:Nf-2) = exp_mio(k,1:N-1);
-    Datas(k+1,Nf-1) = exp_Sx(k,1);
+    Datas(k+1,Nz+1) = exp_Sx(k,1); 
+    Datas(k+1,Nz+2:Nz+12) = exp_mio(k,1:N-1);
+    Datas(k+1,Nz+13) = Energy_gap(k,1);
+    Datas(k+1,Nz+14:Nz+24) = Entanglemennt_Entropy(k,:);
+    Datas(k+1,Nz+25:Nf-1) = G_Data(k,:);
+    
 end
 Datas(2:Ns+1,Nf) = y;
 
 %% plots:
-%% (SzSz correlation functions)
 figure(1)
 x_plot=linspace(1,Nz,Nz);
-plot(x_plot,corr_SzSz(2,:),'-*');
-legend('h=0.1')
+plot(x_plot,corr_SzSz(6,:),'-*');
+legend('h=0.5')
 hold on
-plot(x_plot,corr_SzSz(6,:),'-*','DisplayName','h=0.5');plot(x_plot,corr_SzSz(9,:),'-*','DisplayName','h=0.8');
-plot(x_plot,corr_SzSz(11,:),'-*','DisplayName','h=1');plot(x_plot,corr_SzSz(13,:),'-*','DisplayName','h=1.2');
+plot(x_plot,corr_SzSz(11,:),'-*','DisplayName','h=1');
 plot(x_plot,corr_SzSz(16,:),'-*','DisplayName','h=1.5');plot(x_plot,corr_SzSz(21,:),'-*','DisplayName','h=2');
 plot(x_plot,corr_SzSz(31,:),'-*','DisplayName','h=3');plot(x_plot,corr_SzSz(41,:),'-*','DisplayName','h=4');
 plot(x_plot,corr_SzSz(51,:),'-*','DisplayName','h=5');hold all
 t=title('Correlation Functions(S_zS_z)');
 xlim([1 Nz])
-ylabel('Correlation functions');
+ylabel({'$<\sigma_z^i \sigma_z^{i+1}> - <\sigma_z^i><\sigma_z^{i+1}>$'},'Interpreter','latex','FontSize',15,'FontWeight','bold');
 xlabel('Number of sites');
-grid on
 
 %% (SzSz correlation functions for l = 1)
 figure(2)
 x_plot=linspace(1,N-1,N-1);
-plot(x_plot,corr_SzSz(2,1:N-1),'-*');
-legend('h=0.1')
+plot(x_plot,corr_SzSz(6,1:N-1),'-s','MarkerEdgeColor',[0, 0.4470, 0.7410],'MarkerFaceColor',[0, 0.4470, 0.7410]);
+legend('h=0.5')
 hold on
-plot(x_plot,corr_SzSz(6,1:N-1),'-*','DisplayName','h=0.5');plot(x_plot,corr_SzSz(9,1:N-1),'-*','DisplayName','h=0.8');
-plot(x_plot,corr_SzSz(11,1:N-1),'-*','DisplayName','h=1');plot(x_plot,corr_SzSz(13,1:N-1),'-*','DisplayName','h=1.2');
-plot(x_plot,corr_SzSz(16,1:N-1),'-*','DisplayName','h=1.5');plot(x_plot,corr_SzSz(21,1:N-1),'-*','DisplayName','h=2');
-plot(x_plot,corr_SzSz(31,1:N-1),'-*','DisplayName','h=3');plot(x_plot,corr_SzSz(41,1:N-1),'-*','DisplayName','h=4');
-plot(x_plot,corr_SzSz(51,1:N-1),'-*','DisplayName','h=5');hold all
-t=title('Correlation Functions(S_zS_z) for site 1');
+plot(x_plot,corr_SzSz(9,1:N-1),'-s','MarkerEdgeColor',[0.9290, 0.6940, 0.1250],'MarkerFaceColor',[0.9290, 0.6940, 0.1250],'DisplayName','h=0.8');
+plot(x_plot,corr_SzSz(11,1:N-1),'-s','MarkerEdgeColor',[0.8500 0.3250 0.0980],'MarkerFaceColor',[0.8500 0.3250 0.0980],'DisplayName','h=1');
+plot(x_plot,corr_SzSz(13,1:N-1),'-s','MarkerEdgeColor',[0.4660 0.6740 0.1880],'MarkerFaceColor',[0.4660 0.6740 0.1880],'DisplayName','h=1.2');
+plot(x_plot,corr_SzSz(16,1:N-1),'-s','MarkerEdgeColor',	[0.3010, 0.7450, 0.9330],'MarkerFaceColor',[0.3010, 0.7450, 0.9330],'DisplayName','h=1.5');
+plot(x_plot,corr_SzSz(21,1:N-1),'-s','MarkerEdgeColor',[0.75, 0, 0.75],'MarkerFaceColor',[0.75, 0, 0.75],'DisplayName','h=2');
+plot(x_plot,corr_SzSz(31,1:N-1),'-s','MarkerEdgeColor',	[0.6350, 0.0780, 0.1840],'MarkerFaceColor',[0.6350, 0.0780, 0.1840],'DisplayName','h=3');
+plot(x_plot,corr_SzSz(41,1:N-1),'-s','MarkerEdgeColor',[0, 0.5, 0],'MarkerFaceColor',[0, 0.5, 0],'DisplayName','h=4');
+plot(x_plot,corr_SzSz(51,1:N-1),'-s','MarkerEdgeColor',[0, 0.75, 0.75],'MarkerFaceColor',[0, 0.75, 0.75],'DisplayName','h=5');hold all
+t=title('Correlation Functions');
 xlim([1 N-1])
-ylabel('Correlation functions');
+ylabel({'$<\sigma_z^1 \sigma_z^{i+1}> - <\sigma_z^1><\sigma_z^{i+1}>$'},'Interpreter','latex','FontSize',15,'FontWeight','bold');
 xlabel('Number of sites');
-grid on
 
-%% (expectation values for Sx)
+%% (expectation values for mio_x)
 figure(3)
-h_plot=linspace(0,last_h,Ns);
-plot(h_plot,exp_Sx(:,1),'-*');
-t=title('Expectation Value(S_x)');
-xlim([0 last_h])
-ylabel('<S_x>');
-xlabel('h');
-grid on
-
-%% (expectation values for mio_x (vs sites))
-figure(4)
 x_plot=linspace(1,N-1,N-1);
-plot(x_plot,exp_mio(2,1:N-1),'-*');
-legend('h=0.1')
+plot(x_plot,exp_mio(6,1:N-1),'-s','MarkerEdgeColor',[0, 0.4470, 0.7410],'MarkerFaceColor',[0, 0.4470, 0.7410]);
+legend('h=0.5')
 hold on
-plot(x_plot,exp_mio(9,1:N-1),'-*','DisplayName','h=0.8');plot(x_plot,exp_mio(11,1:N-1),'-*','DisplayName','h=1');
-plot(x_plot,exp_mio(13,1:N-1),'-*','DisplayName','h=1.2');plot(x_plot,exp_mio(16,1:N-1),'-*','DisplayName','h=1.5');
-plot(x_plot,exp_mio(21,1:N-1),'-*','DisplayName','h=2');plot(x_plot,exp_mio(31,1:N-1),'-*','DisplayName','h=3');
-plot(x_plot,exp_mio(41,1:N-1),'-*','DisplayName','h=4');plot(x_plot,exp_mio(51,1:N-1),'-*','DisplayName','h=5');
+plot(x_plot,exp_mio(9,1:N-1),'-s','MarkerEdgeColor',[0.75, 0.75, 0],'MarkerFaceColor',[0.75, 0.75, 0],'DisplayName','h=0.8');
+plot(x_plot,exp_mio(11,1:N-1),'-s','MarkerEdgeColor',[0.8500, 0.3250, 0.0980],'MarkerFaceColor',[0.8500, 0.3250, 0.0980],'DisplayName','h=1');
+plot(x_plot,exp_mio(16,1:N-1),'-s','MarkerEdgeColor',[0.75, 0, 0.75],'MarkerFaceColor',	[0.75, 0, 0.75],'DisplayName','h=1.5');
+plot(x_plot,exp_mio(21,1:N-1),'-s','MarkerEdgeColor',[0.3010, 0.7450, 0.9330],'MarkerFaceColor',[0.3010, 0.7450, 0.9330],'DisplayName','h=2');
+plot(x_plot,exp_mio(31,1:N-1),'-s','MarkerEdgeColor',[0.6350, 0.0780, 0.1840],'MarkerFaceColor',[0.6350, 0.0780, 0.1840],'DisplayName','h=3');
+plot(x_plot,exp_mio(41,1:N-1),'-s','MarkerEdgeColor',[0, 0.75, 0.75],'MarkerFaceColor',[0, 0.75, 0.75],'DisplayName','h=4');
+plot(x_plot,exp_mio(51,1:N-1),'-s','MarkerEdgeColor',[0.9290, 0.6940, 0.1250],'MarkerFaceColor',[0.9290, 0.6940, 0.1250],'DisplayName','h=5');
 hold all
-t=title('Expectation Value(mio_l)');
 xlim([1 N-1])
-ylabel('<mio_l>');
+ylabel('$<\mu_l>$','Interpreter','latex','FontSize',15,'FontWeight','bold');
 xlabel('Number of sites');
-grid on
 
-%% (expectation values for mio_x (vs h))
-figure(5)
+%% (expectation values for mio_x)
+figure(4)
 h_plot=linspace(0,last_h,Ns);
 plot(h_plot,exp_mio(:,1),'-*');
 legend('l=1');
@@ -179,22 +195,61 @@ plot(h_plot,exp_mio(:,4),'-*','DisplayName','l=4');plot(h_plot,exp_mio(:,5),'-*'
 plot(h_plot,exp_mio(:,6),'-*','DisplayName','l=6');plot(h_plot,exp_mio(:,7),'-*','DisplayName','l=7');
 plot(h_plot,exp_mio(:,8),'-*','DisplayName','l=8');plot(h_plot,exp_mio(:,9),'-*','DisplayName','l=9');
 plot(h_plot,exp_mio(:,10),'-*','DisplayName','l=10');plot(h_plot,exp_mio(:,11),'-*','DisplayName','l=11');
-plot(h_plot,exp_mio(:,12),'-*','DisplayName','l=12');plot(h_plot,exp_mio(:,13),'-*','DisplayName','l=13');
 hold all
-t=title('Expectation Value(mio)');
 xlim([0 last_h])
-ylabel('<mio_l>');
+ylabel('$<\mu_l>$','Interpreter','latex','FontSize',15,'FontWeight','bold');
 xlabel('h');
-grid on
+
 %% (SzSz correlation functions vs h)
-figure(6)
+figure(5)
 h_plot=linspace(0,last_h,Ns);
-plot(h_plot,corr_SzSz(:,1),'-*');
-t=title('Correlation Functions(S_zS_z)');
+plot(h_plot,corr_SzSz(:,1),'-s','MarkerEdgeColor','b','MarkerFaceColor',[0, 0.75, 0.75]);
+t=title('Correlation Functions');
 xlim([0 last_h])
-ylabel('Correlation functions');
+ylabel({'$<\sigma_z^1 \sigma_z^{2}> - <\sigma_z^1><\sigma_z^{2}>$'},'Interpreter','latex','FontSize',15,'FontWeight','bold');
 xlabel('h');
-grid on
+
+%% Entanglemennt_Entropy vs subsystem
+figure(6)
+x_plot = linspace(1,N-1,N-1);
+plot(x_plot,Entanglemennt_Entropy(11,:),'-s','MarkerEdgeColor',[0, 0.4470, 0.7410],'MarkerFaceColor',[0, 0.4470, 0.7410]);
+legend('h=1')
+hold on
+plot(x_plot,Entanglemennt_Entropy(16,:),'-s','MarkerEdgeColor',	[0.8500, 0.3250, 0.0980],'MarkerFaceColor',[0.8500, 0.3250, 0.0980],'DisplayName','h=1.5');
+plot(x_plot,Entanglemennt_Entropy(21,:),'-s','MarkerEdgeColor',[0.4660, 0.6740, 0.1880],'MarkerFaceColor',[0.4660, 0.6740, 0.1880],'DisplayName','h=2');
+plot(x_plot,Entanglemennt_Entropy(31,:),'-s','MarkerEdgeColor',	[0.75, 0, 0.75],'MarkerFaceColor',	[0.75, 0, 0.75],'DisplayName','h=3');
+plot(x_plot,Entanglemennt_Entropy(41,:),'-s','MarkerEdgeColor',[0, 0.75, 0.75],'MarkerFaceColor',[0, 0.75, 0.75],'DisplayName','h=4');
+plot(x_plot,Entanglemennt_Entropy(51,:),'-s','MarkerEdgeColor',[0.9290, 0.6940, 0.1250],'MarkerFaceColor',[0.9290, 0.6940, 0.1250],'DisplayName','h=5');hold all
+t=title('Entanglemennt Entropy');
+xlim([1 N-1])
+ylabel('S');
+xlabel('size of subsystem');
+
+%% Energy gap
+figure(7)
+h_plot=linspace(0,last_h,Ns);
+plot(h_plot,Energy_gap(:,1),'-s','MarkerEdgeColor','b','MarkerFaceColor',[0, 0.75, 0.75]);
+t=title('Energy gap');
+xlim([0 last_h])
+ylabel('|E_2 - E_0|');
+xlabel('h');
+%% Entanglemennt_Entropy
+figure(8)
+h_plot=linspace(0,last_h,Ns);
+plot(h_plot,Entanglemennt_Entropy(:,1),'-*');
+legend('size=1')
+hold on
+plot(h_plot,Entanglemennt_Entropy(:,2),'-*','DisplayName','size=2');plot(h_plot,Entanglemennt_Entropy(:,3),'-*','DisplayName','size=3');
+plot(h_plot,Entanglemennt_Entropy(:,4),'-*','DisplayName','size=4');plot(h_plot,Entanglemennt_Entropy(:,5),'-*','DisplayName','size=5');
+plot(h_plot,Entanglemennt_Entropy(:,6),'-*','DisplayName','size=6');plot(h_plot,Entanglemennt_Entropy(:,7),'-*','DisplayName','size=7');
+plot(h_plot,Entanglemennt_Entropy(:,8),'-*','DisplayName','size=8');plot(h_plot,Entanglemennt_Entropy(:,9),'-*','DisplayName','size=9');
+plot(h_plot,Entanglemennt_Entropy(:,10),'-*','DisplayName','size=10');plot(h_plot,Entanglemennt_Entropy(:,11),'-*','DisplayName','size=11');
+hold all
+t=title('Entanglemennt Entropy');
+xlim([0 last_h])
+ylabel('S');
+xlabel('h');
+%%
 toc
-%% 
 %csvwrite('Data_XY.csv',Datas);
+
